5a8fc451c3595e6d4354cd341b8aea7f620cf613
RamaKrushna Dash <little.rama@gmail.com>
2018-07-06 21:54:48 +0530

    Added basic shopping component stucture

---
+++ RecipeBookProject/src/app/model/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+    
+    //public name: string;
+    //public amount: number;
+
+    /**
+     * Defined constructor with PUBLIC type to replace attribute declaration
+     * @param name 
+     * @param amount 
+     */
+    constructor(public name: string, public amount: number){   }
+}
--- RecipeBookProject/src/app/shopping-list/shopping-list.component.ts
+++ RecipeBookProject/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../model/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Mutter', 150),
+    new Ingredient('Paneer', 100)
+  ];
 
   constructor() { }
 
