ab4a7d0cd66d695ce01a05b3d604f6d7732e6d60
RamaKrushna Dash <little.rama@gmail.com>
2018-07-06 20:00:25 +0530

    Prepared basic component design for recipes.

--- RecipeBookProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBookProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../model/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Mutter Curry", "Veg Mutter Curry", "https://cdn.pixabay.com/photo/2017/05/16/16/44/wood-board-2318325_960_720.jpg"),
+    new Recipe("Chicken Fry", "Non-Veg Checken Fry", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859__340.jpg")
+  ];
 
   constructor() { }
 
