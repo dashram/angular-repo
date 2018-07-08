82a2c9e85f2797c5da944eb969946924a892a5b7
RamaKrushna Dash <little.rama@gmail.com>
2018-07-08 13:46:33 +0530

    Life Cycle Hook of components with Local Reference example.

--- angular-learning-udemy/src/app/databind/server-element/server-element.component.ts
+++ angular-learning-udemy/src/app/databind/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges, 
+  SimpleChanges, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked, 
+  AfterViewInit, 
+  AfterViewChecked, 
+  OnDestroy, 
+  ViewChild, 
+  ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,7 +20,14 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements  OnInit, 
+                                                OnChanges, 
+                                                DoCheck, 
+                                                AfterContentInit,
+                                                AfterContentChecked,
+                                                AfterViewInit,
+                                                AfterViewChecked,
+                                                OnDestroy {
 
   /**
    * Exposing perperty to outside to any parent component hosting the current component
@@ -14,9 +35,48 @@ export class ServerElementComponent implements OnInit {
    */
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  @ViewChild('heading') header: ElementRef;
+
+  constructor() {
+    console.log('1. constructor called...');
+   }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log('2. ngOnChanges called...');
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('3. ngOnInit called...');
+    console.log('Text Content: ###'+this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck(){
+    //Triggers when angular checks for any change in any component
+    console.log('4. ngDoCheck called');
+  }
+
+  ngAfterContentInit(){
+    //it executes only once after content initialization
+    console.log('5. ngAfterContentInit called...')
+  }
+
+  ngAfterContentChecked(){
+    console.log('6. ngAfterContentChecked called...')
+  }
+
+  ngAfterViewInit(){
+    //it executes only once after content initialization
+    console.log('7. ngAfterViewInit called...')
+    console.log('Text Content: ###'+this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(){
+    console.log('8. ngAfterViewChecked called...')
+  }
+
+  ngOnDestroy(){
+    console.log('9. ngOnDestroy called...')
   }
 
 }
