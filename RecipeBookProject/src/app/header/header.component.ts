6e367ecfe869663e63a6e043bfcd973689c9df58
RamaKrushna Dash <little.rama@gmail.com>
2018-07-08 15:11:20 +0530

    Event Binding for the Menu "Recipe" and "Shopping List" with Event emitter.

--- RecipeBookProject/src/app/header/header.component.ts
+++ RecipeBookProject/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  //Event Emitter to lest to click event on Recipe or ShoppingList Menu on Header
+  @Output() featureSelected = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
   }
 
 }
