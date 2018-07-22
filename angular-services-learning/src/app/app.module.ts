c723c36736720a886708923fc41777e972a82dde
RamaKrushna Dash <little.rama@gmail.com>
2018-07-22 17:17:23 +0530

    LearningApp: Added basic files for Routes example.

--- angular-services-learning/src/app/app.module.ts
+++ angular-services-learning/src/app/app.module.ts
@@ -4,23 +4,45 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
 import { AccountComponent } from './account/account.component';
 import { NewAccountComponent } from './new-account/new-account.component';
 import { AccountService } from './services/account.service';
 import { LoggingService } from './services/logging.service';
 
+
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { ServersService } from './servers/servers.service';
+
 @NgModule({
   declarations: [
     AppComponent,
+    
     AccountComponent,
-    NewAccountComponent
+    NewAccountComponent,
+    
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [AccountService, LoggingService],
+  providers: [
+    AccountService, 
+    LoggingService,
+    ServersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
