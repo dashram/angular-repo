a8158d93bb84cbc6d0932fb286c4d25ed078c8f8
RamaKrushna Dash <little.rama@gmail.com>
2018-07-22 14:45:27 +0530

    LearningApp: LoggingService and AccountService with Injectable feature explained

--- angular-services-learning/src/app/new-account/new-account.component.ts
+++ angular-services-learning/src/app/new-account/new-account.component.ts
@@ -1,28 +1,28 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { LoggingService } from '../services/logging.service';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  //providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  //Removed as we use service instead of emitter
+  //@Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   //Injecting service in constructor
-  constructor(private loggingService: LoggingService){
+  constructor(private loggingService: LoggingService,
+              private accountService: AccountService){
 
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountService.addAccount(accountName, accountStatus);
 
     //Moved to newly created service
     //console.log('A server status changed, new status: ' + accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
+    //this.loggingService.logStatusChange(accountStatus);
   }
 }
