54e9166970aaed6d9454eef51ebae27b35741c11
RamaKrushna Dash <little.rama@gmail.com>
2018-07-26 21:40:39 +0530

    LearningApp: WildCard Routing for no routing match.

--- angular-services-learning/src/app/app.module.ts
+++ angular-services-learning/src/app/app.module.ts
@@ -19,7 +19,8 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
-import { Routes, RouterModule } from '@angular/router'
+import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -29,7 +30,9 @@ const appRoutes: Routes = [
   { path: 'servers', component: ServersComponent, children: [
     { path: ':id', component: ServerComponent },
     { path: ':id/edit', component: EditServerComponent },
-  ]},  
+  ]},
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/not-found' }
 ];
 
 @NgModule({
@@ -44,7 +47,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
