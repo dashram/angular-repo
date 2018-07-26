5b60685bef0b3ac9c2562eea8e9123c87663d9b5
RamaKrushna Dash <little.rama@gmail.com>
2018-07-26 21:17:12 +0530

    LearningApp: Routing with QueryParam implementation

--- angular-services-learning/src/app/servers/server/server.component.ts
+++ angular-services-learning/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params } from '../../../../node_modules/@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -13,7 +13,8 @@ export class ServerComponent implements OnInit {
 
   constructor(
     private serversService: ServersService,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
@@ -26,6 +27,10 @@ export class ServerComponent implements OnInit {
         this.server = this.serversService.getServer(+params['id']);
       }
     );
+  }
+
+  onEdit() {
+    this.router.navigate(['edit'], { relativeTo: this.route, queryParamsHandling: 'preserve' } )
   }
 
 }
