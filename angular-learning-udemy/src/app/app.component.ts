513b3af488bb166432b74efee7a3098235ddacea
RamaKrushna Dash <little.rama@gmail.com>
2019-01-19 23:02:32 +0530

    LearningApp: Submit Form with data and print form object

--- angular-learning-udemy/src/app/app.component.ts
+++ angular-learning-udemy/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular6';
 
-  onSubmit(){
-    console.log("Submitted....");
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 }
