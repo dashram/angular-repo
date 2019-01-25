b43a0677adfcb6ac574f8b301cc6c5f3862b3574
RamaKrushna Dash <little.rama@gmail.com>
2019-01-25 15:14:06 +0530

    LearningApp: Custom validator and use of ErrorCode to display respective error message.

--- angular-learning-udemy/src/app/form-reactive/form-reactive.component.ts
+++ angular-learning-udemy/src/app/form-reactive/form-reactive.component.ts
@@ -9,13 +9,14 @@ import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 export class FormReactiveComponent implements OnInit {
   genders = ['male', 'Female'];
   signupForm: FormGroup;
+  forbiddenUserName = ['Chris', 'Anna'];
 
   constructor() { }
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        'userName': new FormControl(null, Validators.required),
+        'userName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email]),        
       }),      
       'gender': new FormControl('male'),
@@ -27,6 +28,15 @@ export class FormReactiveComponent implements OnInit {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.signupForm.get("hobbies")).push(control);
 
+  }
+
+  forbiddenNames(control: FormControl): {[s: String]: boolean} {
+    if( this.forbiddenUserName.indexOf(control.value) !== -1){
+      return { 'nameIsForbidden': true }
+    }
+
+    //if validation is false either return null or no return but do not pass with false object.
+    return null;
   }
 
 }
