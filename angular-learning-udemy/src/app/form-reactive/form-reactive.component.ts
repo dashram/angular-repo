a67636ddd3e0c3f40832ac128b184fff64b0982b
RamaKrushna Dash <little.rama@gmail.com>
2019-01-25 20:59:57 +0530

    LearningApp: Asyncronus custom validation and Form Value change and Status Change capture process.

--- angular-learning-udemy/src/app/form-reactive/form-reactive.component.ts
+++ angular-learning-udemy/src/app/form-reactive/form-reactive.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { resolve } from 'url';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-form-reactive',
@@ -17,11 +20,22 @@ export class FormReactiveComponent implements OnInit {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
         'userName': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email]),        
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),        
       }),      
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+
+    //captures any value change happening on form/control value
+    //Can be subscribed to form or individual form-controls
+    /*this.signupForm.valueChanges.subscribe(
+      (value) => console.log(value)
+    )*/
+    //captures any status change happening on form/control status
+    this.signupForm.statusChanges.subscribe(
+      (status) => console.log(status)
+    )
+
   }
 
   onAddHobbies() {
@@ -34,9 +48,22 @@ export class FormReactiveComponent implements OnInit {
     if( this.forbiddenUserName.indexOf(control.value) !== -1){
       return { 'nameIsForbidden': true }
     }
-
     //if validation is false either return null or no return but do not pass with false object.
     return null;
+  }
+
+  //Asyncronous validation (can be used in webservice calls)
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>( (resolve, reject) => {
+      setTimeout(() => {
+        if( control.value === 'test@test.com'){
+          resolve({emailIsForbideen: true})
+        } else {
+          resolve(null)
+        }
+      }, 1500);
+    })
+    return promise;
   }
 
 }
