c36564f77eb5be6e0224f84875ea2af43783855f
RamaKrushna Dash <little.rama@gmail.com>
2019-02-14 23:08:01 +0530

    RecipeBook: Edit Recipe with Reactive form approach: Cancel and refinements with Router Navigation

--- RecipeBookUdemy/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ RecipeBookUdemy/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 import { RecipeService } from '../recipe.service';
@@ -15,7 +15,9 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor( private route: ActivatedRoute, 
+    private recipeService: RecipeService,
+    private router: Router) { }
 
   ngOnInit() {    
     this.route.params
@@ -74,6 +76,7 @@ export class RecipeEditComponent implements OnInit {
     } else {
       this.recipeService.addRecipe( newRecipe );
     }
+    this.onCancel();
   }
 
   onAddIngredient(){
@@ -86,6 +89,10 @@ export class RecipeEditComponent implements OnInit {
         ])
       })
     )
+  }
+
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route})
   }
 
 }
